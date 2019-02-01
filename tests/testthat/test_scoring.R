dca7a4a16edeec778671840262cb5e19f96a9874
saskiaotto <saskia.otto@uni-hamburg.de>
2019-02-01 13:23:05 +0100

    adjusting the new nrmse crit values and summary_sc changes

--- tests/testthat/test_scoring.R
+++ tests/testthat/test_scoring.R
@@ -152,15 +152,15 @@ test_that("test dealing with NAs", {
   expect_equal(sum(test3$press_spec_sc[[2]][4:9]),
     0)
   expect_equal(sum(test4$press_spec_sc[[1]][4:9]),
-    5)  # the sign. model unaffected
+    3)  # the sign. model unaffected
   expect_equal(sum(test4$press_spec_sc[[2]][4:9]),
     0)
   expect_equal(sum(test5$press_spec_sc[[1]][4:9]),
-    5)  # the sign. model unaffected
+    3)  # the sign. model unaffected
   expect_equal(sum(test5$press_spec_sc[[2]][4:9]),
     0)
   expect_equal(sum(test6$press_spec_sc[[1]][4:9]),
-    3)  # only 9.1 should be zero now
+    1)  # only 9.1 should be zero now
   expect_equal(sum(test6$press_spec_sc[[2]][4:9]),
     0)
 
@@ -178,38 +178,3 @@ test_that("test dealing with NAs", {
 
 
 # Remove criteria
-crit_scores_mod3 <- crit_scores_tmpl[crit_scores_tmpl$crit %in%
-  c("C8", "C9", "C10") & crit_scores_tmpl$subcrit !=
-  "C10_3", ]
-dat2 <- scoring(trend_tbl = model_trend_ex, mod_tbl = all_results_ex,
-  press_type = INDperform::press_type_ex, crit_scores = crit_scores_mod3)
-dat2 <- tidyr::unnest(dat2)
-ind_v <- unique(dat2$ind)
-subcrit_v <- c("C9_1", "C9_2", "C10_1", "C10_2", "C10_4")
-
-dat <- scoring(trend_tbl = model_trend_ex, mod_tbl = all_results_ex,
-  press_type = INDperform::press_type_ex)
-dat <- tidyr::unnest(dat)
-ind_v <- unique(dat$ind)
-crit_v <- names(dat) %in% c("C8", "C11")
-subcrit_v <- c("C9_1", "C9_2", "C10_1", "C10_2", "C10_3",
-  "C10_4")
-
-test_that("test structure of returned object", {
-  expect_is(dat2, "tbl_df")
-  # all ind and press included?
-  expect_identical(sort(unique(dat2$ind)), sort(unique(model_trend_ex$ind)))
-  expect_identical(sort(unique(dat2$ind)), sort(unique(all_results_ex$ind)))
-  expect_identical(sort(unique(dat2$press)), sort(unique(all_results_ex$press)))
-  expect_identical(sort(unique(dat2$press)), sort(unique(all_results_ex$press)))
-  # all (sub)crit included?
-  expect_equal(sum(names(dat2) %in% c("C8", "C11")),
-    1)
-  expect_equal(sum(subcrit_v %in% names(dat2)), 5)
-})
-
-
-test_that("test scores", {
-  expect_lte(max(dat2[, c(2, 6:10)]), 3)
-  expect_true(sum(is.na(dat2[, c(2, 6:10)])) == 0)
-})
