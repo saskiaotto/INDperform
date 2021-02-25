963c668ed5ec1b1ed82d28d06c397025c99a6d08
Otto, Dr. Saskia <saskia.otto@uni-hamburg.de>
2021-02-25 09:03:49 +0100

    added summary in example to make more sense of the resulting tibble

--- R/scoring.R
+++ R/scoring.R
@@ -79,6 +79,7 @@
 #' # Using the Baltic Sea demo data in this package
 #' scores_tbl <- scoring(trend_tbl = model_trend_ex, mod_tbl = all_results_ex,
 #'   press_type = press_type_ex)
+#' summary_sc(scores_tbl)
 #' \dontrun{
 #'  # To see the criterion template and change it potentially
 #'  View(crit_scores_tmpl)
